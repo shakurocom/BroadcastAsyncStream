04c4e4340574b3f533b62c1bb220d70458bcb409
Sergey Popov <spopov@shakuro.com>
2024-12-11 21:57:37 +0300

    II-15-[added examples and package]

---
+++ Examples/SwiftUI/BroadcastAsyncStreamExample/BroadcastAsyncStreamExample/BroadcastAsyncStreamExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastAsyncStreamExampleApp.swift
+//  BroadcastAsyncStreamExample
+//
+//  Created by Sergey Popov on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BroadcastAsyncStreamExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/UIKit/BroadcastAsyncStreamExample/BroadcastAsyncStreamExampleTests/BroadcastAsyncStreamExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastAsyncStreamExampleTests.swift
+//  BroadcastAsyncStreamExampleTests
+//
+//  Created by Sergey Popov on 12/11/24.
+//
+
+import Testing
+@testable import BroadcastAsyncStreamExample
+
+struct BroadcastAsyncStreamExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
